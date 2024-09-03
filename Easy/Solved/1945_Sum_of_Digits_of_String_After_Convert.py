25148ac5b3805082f62b7b5d9bee71391ef5eeb7
Gjiha <franci2002.fc@gmail.com>
2024-09-03 22:48:46 +0200

    aggiunta di un nuovo esercizio

---
+++ Easy/Solved/1945_Sum_of_Digits_of_String_After_Convert.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        newString = ''
+        for char in s:
+            newString += str(ord(char) -96)
+        
+        result = 0
+
+        while k > 0:
+            for digit in newString:
+                result += int(digit)
+            newString = str(result)
+            k -= 1
+        
+        return result
+
+
+
+sol = Solution()
+print(sol.getLucky('leetcode',2))
