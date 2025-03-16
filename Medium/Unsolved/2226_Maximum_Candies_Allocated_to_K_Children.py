042781ecefbc136a805bdcae81ef2e4eb8e97984
Gjiha <franci2002.fc@gmail.com>
2025-03-16 12:45:11 +0100

    aggiunta di un esercizio

---
+++ Medium/Unsolved/2226_Maximum_Candies_Allocated_to_K_Children.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def checkSol(self, list:list[int], input)
+    def binSearch(self, list:list[int], start:int, end:int, input:int)-> int:
+
+
+    def maximumCandies(self, candies: list[int], k: int) -> int:
+        maxCandies = sum(candies)//k
+        candies.sort()
+
+
