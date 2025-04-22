749562c0dbc29e5229ed6ce6a2f7bbe678e9c2eb
Gjiha <franci2002.fc@gmail.com>
2025-04-22 12:03:14 +0200

    aggiunta es

---
+++ albero_nlog.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def pathSum(self, root, targetSum: int) -> int:
+        currentSum = 0
+        dictOfSums = {0:1}
+        count = self.dfsRic(root, targetSum, currentSum, dictOfSums)
+        return count
+    
+    def dfsRic(self, node, targetSum: int, currentSum:int, dictOfSums:dict[int:int]) -> int:
+        if node == None:
+            return 0
+        
+        count = 0
+
+        currentSum += node.val 
+
+        if currentSum - targetSum in dictOfSums:
+            count += dictOfSums[currentSum - targetSum]
+
+        if currentSum not in dictOfSums:
+            dictOfSums[currentSum] = 1
+        else:
+            dictOfSums[currentSum] += 1
+
+        count += self.dfsRic(node.left, targetSum, currentSum, dictOfSums)
+        count += self.dfsRic(node.right, targetSum, currentSum, dictOfSums)
+
+        dictOfSums[currentSum] -= 1        
+
+        return count
